efaac04713f75a41777ccdda4e670d64ee03a56d
Babang <mubarak.m@gojek.com>
2025-12-06 12:58:55 +0700

    show Polymorphic method

--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,33 @@ public class Main {
         Book book3 = new Book("JavaScript for dummies", "Doug Lowe");
         Book book4 = new Book("SQL for dummies", "Allen G. Taylor");
 
-        Admin admin = new Admin("Strygwyr the Bloodseeker");
-        Member member = new Member("Kardel");
+        User admin = new Admin("Strygwyr the Bloodseeker");
+        User member = new Member("Kardel");
 
         System.out.println("\n### call interact()\n");
-        admin.interact();
-        member.interact();
+        admin.interact();// Polymorphism
+        member.interact();//Polymorphism
         System.out.println("\n### admin adding books\n");
-        admin.addBook(books, book0);
-        admin.addBook(books, book1);
-        admin.addBook(books, book2);
-        admin.addBook(books, book3);
-        admin.addBook(books, book4);
+        ((Admin) admin).addBook(books, book0);//need to cast as Admin to use admin method
+        ((Admin) admin).addBook(books, book1);
+        ((Admin) admin).addBook(books, book2);
+        ((Admin) admin).addBook(books, book3);
+        ((Admin) admin).addBook(books, book4);
 
         System.out.println("\n### admin removing books\n");
-        admin.removeBook(books, "SQL for dummies");
+        ((Admin) admin).removeBook(books, "SQL for dummies");
 
         System.out.println("\n### member borrowing books\n");
-        member.borrowBook(book0);
-        member.borrowBook(book2);
+        ((Member) member).borrowBook(book0);// need to cast as Member to use member method
+        ((Member) member).borrowBook(book2);
 
         System.out.println("\n### member returning books\n");
-        member.returnBook(book0);
+        ((Member) member).returnBook(book0);
 
         System.out.println("\n### admin trying to find book by title\n");
-        admin.findBookByTitle(books,"java for dummies");
+        ((Admin) admin).findBookByTitle(books, "java for dummies");
 
         System.out.println("\n### admin checking available books\n");
-        admin.showAvailableBooks(books);
+        ((Admin) admin).showAvailableBooks(books);
     }
 }
